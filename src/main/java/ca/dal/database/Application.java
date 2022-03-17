7211d9e33e3edd8688a68ef9abbcf0a36cb26c33
handsomecoder <harshshah1295@gmail.com>
2022-03-17 01:53:18 -0300

    Initial Commit

---
+++ src/main/java/ca/dal/database/Application.java
@@ -0,0 +1,29 @@
+package ca.dal.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
+/**
+ * @author Harsh Shah
+ */
+public class Application {
+
+    static {
+        setup();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void setup() {
+        try {
+            InputStream loggerProps = ClassLoader.getSystemResourceAsStream("logger.properties");
+            LogManager.getLogManager().readConfiguration(loggerProps);
+        } catch (SecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
