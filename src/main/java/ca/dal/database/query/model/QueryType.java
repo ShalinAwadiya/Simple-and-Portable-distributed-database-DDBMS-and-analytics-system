7a672166d60f668ccc02a346ad2545117fd3f1a1
Nishit Mistry <ns209086@dal.ca>
2022-04-07 01:09:31 -0300

    Setter Getter for QueryParser

---
+++ src/main/java/ca/dal/database/query/model/QueryType.java
@@ -0,0 +1,8 @@
+package ca.dal.database.query.model;
+
+public enum QueryType {
+    CREATE_DATABASE, USE_DATABASE,
+    CREATE_TABLE,
+    INSERT_ROW, SELECT_ROW, UPDATE_ROW, DELETE_ROW,
+    START_TRANSACTION, END_TRANSACTION, COMMIT, ROLLBACK
+}
