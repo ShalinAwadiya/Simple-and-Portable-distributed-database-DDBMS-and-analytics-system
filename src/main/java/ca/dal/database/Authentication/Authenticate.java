6a5be9e66dfc938a8582437a30c7d3a64ad7e132
Kavya <kv286760@dal.ca>
2022-04-07 22:17:31 -0300

    authentication feature added

---
+++ src/main/java/ca/dal/database/Authentication/Authenticate.java
@@ -0,0 +1,149 @@
+package ca.dal.database.Authentication;
+
+import java.util.Scanner;
+
+public class Authenticate {
+    public void init() {
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("3. Exit");
+        System.out.print("Enter Your Choice: ");
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+        switch (userInput) {
+            case "1":
+                userRegistration();
+                break;
+            case "2":
+                userLogin();
+                break;
+            case "3":
+                System.out.println("Good Bye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Incorrect option chosen, Please try Again");
+                init();
+                break;
+        }
+    }
+
+    private void userRegistration() {
+        Scanner sc = new Scanner(System.in);
+        User u = new User();
+
+        boolean isUserIdCorrect = false;
+        while (!isUserIdCorrect) {
+            System.out.println("");
+            System.out.print("Enter UserId: ");
+            String userId = sc.nextLine();
+            if (userId.length() < 1) {
+                System.out.println("userId cannot be empty.");
+                continue;
+            }
+            if (u.userIdCheck(userId)) {
+                System.out.println("userId already exists.");
+                continue;
+            }
+            isUserIdCorrect = true;
+            u.setUserId(userId);
+        }
+
+        boolean isPasswordCorrect = false;
+        while (!isPasswordCorrect) {
+            System.out.println("");
+            System.out.print("Enter Password: ");
+            String password = sc.nextLine();
+
+            if (password.length() < 1) {
+                System.out.println("password cannot be empty");
+                continue;
+            }
+            isPasswordCorrect = true;
+            u.setPassword(password);
+        }
+
+        boolean isSecurityCorrect = false;
+        while (!isSecurityCorrect) {
+            System.out.println("");
+            System.out.print("Enter Security Question: ");
+            String securityQuestion = sc.nextLine();
+            if (securityQuestion.length() < 1) {
+                System.out.println("Security Question cannot be empty");
+                continue;
+            }
+            isSecurityCorrect = true;
+            u.setSecurityQuestion(securityQuestion);
+        }
+
+        boolean isAnswerCorrect = false;
+        while (!isAnswerCorrect) {
+            System.out.println("");
+            System.out.print("Enter Answer: ");
+            String answer = sc.nextLine();
+            if (answer.length() < 1) {
+                System.out.println("Answer cannot be empty");
+            }
+            isAnswerCorrect = true;
+            u.setAnswer(answer);
+        }
+
+        u.save();
+    }
+
+    private void userLogin() {
+        Scanner sc = new Scanner(System.in);
+        String userId = "";
+        String password = "";
+
+        boolean isUserIdCorrect = false;
+        while (!isUserIdCorrect) {
+            System.out.println("");
+            System.out.print("Enter UserId: ");
+            userId = sc.nextLine();
+            if (userId.length() < 1) {
+                System.out.println("userId cannot be empty.");
+                continue;
+            }
+            isUserIdCorrect = true;
+        }
+
+        boolean isPasswordCorrect = false;
+        while (!isPasswordCorrect) {
+            System.out.println("");
+            System.out.print("Enter Password: ");
+            password = sc.nextLine();
+            if (password.length() < 1) {
+                System.out.println("password cannot be empty");
+                continue;
+            }
+            isPasswordCorrect = true;
+        }
+
+        User user = new User();
+        user = user.validateUserIdAndPassword(userId, password);
+        if (user == null) {
+            System.out.println("Incorrect UserId or Password");
+            init();
+            return;
+        }
+
+        boolean isAnswerCorrect = false;
+        String answer = "";
+        while (!isAnswerCorrect) {
+            System.out.println("");
+            System.out.print("Enter Answer for " + user.getSecurityQuestion() + ": ");
+            answer = sc.nextLine();
+            if (answer.length() < 1) {
+                System.out.println("Answer cannot be empty");
+            }
+            isAnswerCorrect = true;
+        }
+        if (!user.getAnswer().equalsIgnoreCase(answer)) {
+            System.out.println("Invalid Security");
+            return;
+        }
+    }
+}
