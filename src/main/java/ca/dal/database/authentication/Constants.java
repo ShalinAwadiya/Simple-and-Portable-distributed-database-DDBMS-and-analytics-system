49b7d28a74b7067d6f3b11aadca040aa840c9891
Nishit Mistry <ns209086@dal.ca>
2022-04-07 22:34:34 -0300

    integration

---
+++ src/main/java/ca/dal/database/authentication/Constants.java
@@ -0,0 +1,6 @@
+package ca.dal.database.authentication;
+
+public class Constants {
+    final static String USER_PROFILE_TXT = "src/main/java/Authentication/UserProfile.txt";
+    final static String delimiter = "<!>";
+}
